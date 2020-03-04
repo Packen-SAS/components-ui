f5eaa42af834f3811b49a8230fab9464a14e8c22
Jhampier Giron <jose.giron@packen.co>
2020-03-04 17:55:28 -0500

    Se termina la funcionalidad y diseño de los input, faltan las pruebas unitarias

---
+++ app/views/Input.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import PackenInput from '../components/PackenInput';
+import PackenText from '../components/PackenText';
+import Section from '../components/Section';
+import Typography from '../styles/abstracts/typography';
+import Colors from '../styles/abstracts/colors';
+
+
+
+class Inputs extends React.Component {
+
+    state = {
+        errorMessage: 'Mensaje de error',
+        value: 'Value'
+    }
+
+
+    notifyParent = (value) => {
+        this.setState({ value: value, errorMessage: '' });
+        
+    }
+
+    render() {
+        return (
+            <View>
+                <Section title="Inputs">
+                    <View  >
+                        <PackenInput
+                            placeholder="Placeholder"
+                            errorMessage={this.state.errorMessage}
+                            icon="lock"
+                            value={this.state.value}
+                            label="Input error"
+                            notifyParent={this.notifyParent} 
+                            />
+                    </View>
+
+                    <View >
+                        <PackenInput
+                            placeholder="Placeholder"
+                            icon="lock"
+                            label="Disabled"
+                            disabled={true} />
+                    </View>
+                </Section>
+                <Button onPress={() => this.setState({ errorMessage: "Mensaje de error" })} title="Cambiar estado" />
+            </View>
+
+
+        )
+    }
+}
+
+export default Inputs;
+
