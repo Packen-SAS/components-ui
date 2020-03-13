abdeece81992cfa11078eccdcba4ba5492afdf84
Andrés Liu <andres.liu@packen.co>
2020-03-13 09:58:38 -0500

    Added callback to PackenRadio, so the new selection can be accessed from the component

--- app/components/PackenRadio.js
+++ app/components/PackenRadio.js
@@ -38,6 +38,7 @@ class PackenRadio extends Component {
     if (prevState.currentSelection !== this.state.currentSelection) {
       /* New selection can be used here */
       /* console.log(this.state.currentSelection); */
+      this.props.callback(this.state.currentSelection);
       return this.state.currentSelection;
     }
   }
