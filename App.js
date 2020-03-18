e061ac2569dae22aea8bd6e37e499f58851cde84
Andrés Liu <andres.liu@packen.co>
2020-03-18 08:29:00 -0500

    Set App.js to display Storybook

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 
 import UILibrary from "./app/UILibrary"
-/* import StorybookUIRoot from "./storybook"; */
+import StorybookUIRoot from "./storybook";
 
-export default class App extends Component {
+/* export default class App extends Component {
   render() {
     return (
       <UILibrary/>
     );
   }
-}
+} */
 
-/* export default StorybookUIRoot; */
+export default StorybookUIRoot;
