3c95c46ecc9ac4e7d7d9af845c16beaa5b199235
Andrés Liu <andres.liu@packen.co>
2020-03-18 08:27:45 -0500

    Set App.js to default display

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import React, {Component} from 'react';
 import UILibrary from "./app/UILibrary"
 /* import StorybookUIRoot from "./storybook"; */
 
-/* export default class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <UILibrary/>
     );
   }
-} */
+}
 
-export default StorybookUIRoot;
+/* export default StorybookUIRoot; */
