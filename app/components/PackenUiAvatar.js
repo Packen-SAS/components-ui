af1ac0de1b8cf7e6b5c55cbaed03c87f769cbdfb
Andrés Liu <andres.liu@packen.co>
2020-04-08 16:03:36 -0500

    Updated Avatar component to support empty states and optional touchable callbacks. Updates tests as well.

--- app/components/PackenUiAvatar.js
+++ app/components/PackenUiAvatar.js
@@ -1,26 +1,94 @@
 import React, { Component } from "react";
+import { View, Image, TouchableWithoutFeedback } from "react-native";
 
-import { View, Image } from "react-native";
+import Icon from "react-native-vector-icons/dist/Feather";
+
+import Colors from "../styles/abstracts/colors";
 
 class PackenUiAvatar extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      src: props.src,
+      size: props.size,
+      callback: props.callback
+    }
   }
 
-  render() {
-    return (
-      <View style={[
-        this.getStyles().container.base,
-        this.getStyles().container.size[this.props.size]
-      ]}>
+  getInner = () => {
+    let inner = null;
+
+    if (this.state.src) {
+      inner = (
         <Image
-          source={this.props.src}
+          source={this.state.src}
           style={{
             height: "100%",
             width: "100%",
             borderRadius: 100
           }}
         />
+      );
+    } else {
+      inner = (
+        <Icon
+          name="camera"
+          size={this.getStyles().container.size[this.state.size].width / 3}
+          color={Colors.basic.independence.lgt}
+        />
+      );
+    }
+
+    return inner;
+  }
+
+  getContent = () => {
+    let content = null;
+
+    if (this.state.callback) {
+      content = (
+        <TouchableWithoutFeedback onPress={this.state.callback} >
+          <View
+            style={[
+              this.getStyles().touchable,
+              {
+                height: this.getStyles().container.size[this.state.size.height],
+                width: this.getStyles().container.size[this.state.size.width]
+              }
+            ]}
+          >
+            {this.getInner()}
+          </View>
+        </TouchableWithoutFeedback>
+      )
+    } else {
+      content = this.getInner();
+    }
+
+    return content;
+  }
+
+  updateState = () => {
+    this.setState({
+      src: this.props.src,
+      size: this.props.size
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (JSON.stringify(prevProps) !== JSON.stringify(this.props)) {
+      this.updateState();
+    }
+  }
+
+  render() {
+    return (
+      <View style={[
+        this.getStyles().container.base,
+        this.getStyles().container.size[this.state.size]
+      ]}>
+        {this.getContent()}
       </View>
     );
   }
@@ -30,7 +98,8 @@ class PackenUiAvatar extends Component {
       container: {
         base: {
           borderRadius: 100,
-          overflow: "hidden"
+          overflow: "hidden",
+          backgroundColor: Colors.basic.gray.lgt
         },
         size: {
           tiny: {
@@ -54,6 +123,12 @@ class PackenUiAvatar extends Component {
             width: 96
           }
         }
+      },
+      touchable: {
+        flex: 1,
+        borderRadius: 100,
+        alignItems: "center",
+        justifyContent: "center"
       }
     };
   }
--- app/views/Avatars.js
+++ app/views/Avatars.js
@@ -3,7 +3,6 @@ import { View } from "react-native";
 
 import Section from "../components/Section";
 
-
 import PackenUiAvatar from "../components/PackenUiAvatar";
 
 class Avatars extends Component {
@@ -11,23 +10,50 @@ class Avatars extends Component {
     super(props);
   }
 
+  mockCallback = () => true;
+
   render() {
     return (
       <Section title="Avatars">
-        <View style={{ marginTop: 20 }}>
-          <PackenUiAvatar size="tiny" src={require("../../assets/images/avatar.jpg")}/>
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="tiny" src={require("../../assets/images/avatar.jpg")} />
+          </View>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="tiny" callback={this.mockCallback} />
+          </View>
         </View>
-        <View style={{ marginTop: 20 }}>
-          <PackenUiAvatar size="small" src={require("../../assets/images/avatar.jpg")}/>
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="small" src={require("../../assets/images/avatar.jpg")} />
+          </View>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="small" callback={this.mockCallback} />
+          </View>
         </View>
-        <View style={{ marginTop: 20 }}>
-          <PackenUiAvatar size="medium" src={require("../../assets/images/avatar.jpg")}/>
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="medium" src={require("../../assets/images/avatar.jpg")} />
+          </View>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="medium" callback={this.mockCallback} />
+          </View>
         </View>
-        <View style={{ marginTop: 20 }}>
-          <PackenUiAvatar size="large" src={require("../../assets/images/avatar.jpg")}/>
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="large" src={require("../../assets/images/avatar.jpg")} />
+          </View>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="large" callback={this.mockCallback} />
+          </View>
         </View>
-        <View style={{ marginTop: 20 }}>
-          <PackenUiAvatar size="giant" src={require("../../assets/images/avatar.jpg")}/>
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="giant" src={require("../../assets/images/avatar.jpg")} />
+          </View>
+          <View style={{ marginRight: 10 }}>
+            <PackenUiAvatar size="giant" callback={this.mockCallback} />
+          </View>
         </View>
       </Section>
     );
