f034e74f327adf20a828637a6702475d32a26f7c
Andrés Liu <andres.liu@packen.co>
2020-04-21 17:52:25 -0500

    Updated package.json details, defined the component exports in index.js, and added .npmignore

--- index.js
+++ index.js
@@ -6,4 +6,70 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import PackenUiAvatar from "./app/components/PackenUiAvatar";
+import PackenUiButton from "./app/components/PackenUiButton";
+import PackenUiCheckbox from "./app/components/PackenUiCheckbox";
+import PackenUiCheckboxControl from "./app/components/PackenUiCheckboxControl";
+import PackenUiDivider from "./app/components/PackenUiDivider";
+import PackenUiDropdown from "./app/components/PackenUiDropdown";
+import PackenUiDropdownList from "./app/components/PackenUiDropdownList";
+import PackenUiDropdownListItem from "./app/components/PackenUiDropdownListItem";
+import PackenUiInfoAction from "./app/components/PackenUiInfoAction";
+import PackenUiInput from "./app/components/PackenUiInput";
+import PackenUiList from "./app/components/PackenUiList";
+import PackenUiListItem from "./app/components/PackenUiListItem";
+import PackenUiLoaderButton from "./app/components/PackenUiLoaderButton";
+import PackenUiMapPin from "./app/components/PackenUiMapPin";
+import PackenUiMapPinSub from "./app/components/PackenUiMapPinSub";
+import PackenUiModal from "./app/components/PackenUiModal";
+import PackenUiNotificationBanner from "./app/components/PackenUiNotificationBanner";
+import PackenUiProgressbar from "./app/components/PackenUiProgressbar";
+import PackenUiRadar from "./app/components/PackenUiRadar";
+import PackenUiRadio from "./app/components/PackenUiRadio";
+import PackenUiRadioControl from "./app/components/PackenUiRadioControl";
+import PackenUiSelectionButtons from "./app/components/PackenUiSelectionButtons";
+import PackenUiSelectionButtonsControl from "./app/components/PackenUiSelectionButtonsControl";
+import PackenUiServiceStatus from "./app/components/PackenUiServiceStatus";
+import PackenUiServiceStatusItem from "./app/components/PackenUiServiceStatusItem";
+import PackenUiTabItem from "./app/components/PackenUiTabItem";
+import PackenUiTabs from "./app/components/PackenUiTabs";
+import PackenUiTag from "./app/components/PackenUiTag";
+import PackenUiText from "./app/components/PackenUiText";
+import PackenUiToggle from "./app/components/PackenUiToggle";
+import PackenUiVehicleBox from "./app/components/PackenUiVehicleBox";
+
 AppRegistry.registerComponent(appName, () => App);
+
+export {
+  PackenUiAvatar,
+  PackenUiButton,
+  PackenUiCheckbox,
+  PackenUiCheckboxControl,
+  PackenUiDivider,
+  PackenUiDropdown,
+  PackenUiDropdownList,
+  PackenUiDropdownListItem,
+  PackenUiInfoAction,
+  PackenUiInput,
+  PackenUiList,
+  PackenUiListItem,
+  PackenUiLoaderButton,
+  PackenUiMapPin,
+  PackenUiMapPinSub,
+  PackenUiModal,
+  PackenUiNotificationBanner,
+  PackenUiProgressbar,
+  PackenUiRadar,
+  PackenUiRadio,
+  PackenUiRadioControl,
+  PackenUiSelectionButtons,
+  PackenUiSelectionButtonsControl,
+  PackenUiServiceStatus,
+  PackenUiServiceStatusItem,
+  PackenUiTabItem,
+  PackenUiTabs,
+  PackenUiTag,
+  PackenUiText,
+  PackenUiToggle,
+  PackenUiVehicleBox
+};
