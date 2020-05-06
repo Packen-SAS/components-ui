59932426fc2562f6c0d90967f448f8e396ee5267
Andrés Liu <andres.liu@packen.co>
2020-05-06 10:42:12 -0500

    @andresliu(tests): Removed storybook files from coverage and tests reports

--- storybook/index.js
+++ storybook/index.js
@@ -3,7 +3,6 @@ import { getStorybookUI, configure } from '@storybook/react-native';
 
 import './rn-addons';
 
-// import stories
 configure(() => {
   require("./stories/PackenUiAlert");
   require("./stories/PackenUiAvatar");
@@ -31,12 +30,8 @@ configure(() => {
   require("./stories/PackenUiVehicleBox");
 }, module);
 
-// Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
-// To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({});
 
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
-// If you use Expo you can safely remove this line.
-AppRegistry.registerComponent('ui_library', () => StorybookUIRoot);
+AppRegistry.registerComponent("ui_library", () => StorybookUIRoot);
 
 export default StorybookUIRoot;
