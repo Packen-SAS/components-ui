418a7bc3910543575696348e8802e81ba16a1db4
Andrés Liu <andres.liu@packen.co>
2020-05-12 15:08:40 -0500

    @andresliu(refactor): Standardized shadow level variables for both Android and iOS

--- app/styles/abstracts/shadows.js
+++ app/styles/abstracts/shadows.js
@@ -1,26 +1,62 @@
 export default {
   xs: {
-    elevation: 1,
-    size: .75
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.00,
+    elevation: 1
   },
   sm: {
-    elevation: 3,
-    size: 2.25
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3
   },
   md: {
-    elevation: 4,
-    size: 3
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4
   },
   lg: {
-    elevation: 8,
-    size: 6
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 4.65,
+    elevation: 8
   },
   xl: {
-    elevation: 12,
-    size: 9
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 12
   },
   gt: {
-    elevation: 24,
-    size: 18
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+    elevation: 24
   }
 }
