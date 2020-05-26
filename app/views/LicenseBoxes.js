22cd74328561a3147ca1aeb4c2dbed3cbb6743f3
Andrés Liu <andres.liu@packen.co>
2020-05-26 14:22:59 -0500

    @andresliu(feature): Added samples for testing PackenUiLicenseBox components

---
+++ app/views/LicenseBoxes.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import moment from "moment";
+
+import * as UTIL from "../utils";
+import Section from "../components/Section";
+import PageView from "./PageView";
+
+import PackenUiLicenseBox from "../components/PackenUiLicenseBox";
+
+class LicenseBoxes extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <PageView>
+        <Section title="License Boxes">
+          <View style={{ marginTop: 20 }}>
+            <View style={{ marginBottom: 10 }}>
+              <PackenUiLicenseBox
+                overview="Licencia para camión con refrigeración"
+                category="A1"
+                number="000000"
+                state="approved"
+                dueDate={UTIL.formatDateSimple("2021-08-14", "DD/MM/YYYY")}
+                callback={() => false}
+                labels={{
+                  approved: "Approved",
+                  expired: "Expired",
+                  rejected: "Rejected",
+                  pending: "Pending"
+                }}
+              />
+            </View>
+          </View>
+        </Section>
+      </PageView>
+    );
+  }
+}
+
+export default LicenseBoxes;
