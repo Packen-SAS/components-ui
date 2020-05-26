baa0c7e87f2527fbf192607a9929e58d70eb9bae
Andrés Liu <andres.liu@packen.co>
2020-05-26 14:26:05 -0500

    @andresliu(refactor): Removed unused import

--- app/views/LicenseBoxes.js
+++ app/views/LicenseBoxes.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import moment from "moment";
 
 import * as UTIL from "../utils";
 import Section from "../components/Section";
