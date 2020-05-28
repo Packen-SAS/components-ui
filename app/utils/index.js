952cecfda153103b78f4d95aa5a2484a67f3fe30
Andrés Liu <andres.liu@packen.co>
2020-05-28 15:44:44 -0500

    @andresliu(feature): Added missing util functions from Driver

--- app/utils/index.js
+++ app/utils/index.js
@@ -18,9 +18,78 @@ export const arraysEqual = (_arr1, _arr2) => {
   return true;
 }
 
+const localtime = {
+  0: "00", 1: "01", 2: "02", 3: "03",
+  4: "04", 5: "05", 6: "06", 7: "07",
+  8: "08", 9: "09", 10: "10", 11: "11",
+  12: "12", 13: "01", 14: "02", 15: "03",
+  16: "04", 17: "05", 18: "06", 19: "07",
+  20: "08", 21: "09", 22: "10", 23: "11"
+}
+
+const datetime_locale = {
+  "es": {
+    months: {
+      0: "Enero", 1: "Febrero", 2: "Marzo", 3: "Abril",
+      4: "Mayo", 5: "Junio", 6: "Julio", 7: "Agosto",
+      8: "Septiembre", 9: "Octubre", 10: "Noviembre", 11: "Diciembre"
+    },
+    days: {
+      0: "Domingo", 1: "Lunes", 2: "Martes", 3: "Miércoles",
+      4: "Jueves", 5: "Viernes", 6: "Sábado"
+    }
+  },
+  "en": {
+    months: {
+      0: "January", 1: "Febrary", 2: "March", 3: "April",
+      4: "May", 5: "June", 6: "July", 7: "August",
+      8: "September", 9: "October", 10: "November", 11: "December"
+    },
+    days: {
+      0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday",
+      4: "Thursday", 5: "Friday", 6: "Saturday"
+    }
+  }
+}
+
+export const getTime = date => {
+  date = date instanceof Date ? date : new Date(date);
+  const HH = localtime[date.getHours()];
+  const MM = zeroPaddingForDate(date.getMinutes());
+  const SS = zeroPaddingForDate(date.getSeconds());
+  const TZ = (date.getHours() < 12 ? "AM" : "PM");
+  return `${HH}:${MM}:${SS} ${TZ}`;
+}
+
+const zeroPaddingForDate = day => (day < 10 ? `0${day}` : day);
+
+export const getDate = time => {
+  const date = time instanceof Date ? time : new Date(time);
+  const DD = zeroPaddingForDate(date.getUTCDate());
+  const MO = zeroPaddingForDate(date.getUTCMonth() + 1);
+  const YYYY = date.getFullYear();
+  const result = `${DD}/${MO}/${YYYY} ${getTime(date)}`;
+  return result;
+}
+
+export const getDateByChunks = d => {
+  const date = d instanceof Date ? d : new Date(d);
+  const lang = i18n();
+  const date_local = datetime_locale[lang.locale];
+  return {
+    month: date_local.months[date.getMonth()],
+    day: date_local.days[date.getDay()],
+    nday: zeroPaddingForDate(date.getDate()),
+    year: date.getUTCFullYear(),
+    time: getTime(date),
+    locale: lang.locale,
+    unix: date.getTime()
+  }
+}
+
 export const datetime = () => {
   return {
-    plain: (time) => getDate(time),
+    plain: time => getDate(time),
     parts: time => getDateByChunks(time),
     obj: o => moment(o),
     diff: (a, b, mode) => (a.diff(b, mode))
