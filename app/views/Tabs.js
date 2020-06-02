734c00e9ed16f9fd5a1123cdc8d6f90201f44f1f
Andrés Liu <andres.liu@packen.co>
2020-06-02 11:45:16 -0500

    @andresliu(refactor): Included all use cases for tab triggers in the same example component

--- app/views/Tabs.js
+++ app/views/Tabs.js
@@ -51,13 +51,12 @@ class Tabs extends Component {
             callback: this.mockCallback,
           },
           {
-            icon: "»",
+            icon: "user",
             label: "Avatars",
             view: <Avatars />,
             callback: this.mockCallback
           },
           {
-            icon: "»",
             label: "InfoActions",
             view: <InfoActions />,
             callback: this.mockCallback
@@ -70,87 +69,6 @@ class Tabs extends Component {
         headerComponent={this.header}
         footerComponent={this.footer}
       />
-    );
-    /* Examples for triggers */
-    return (
-      <PageView>
-        <Section title="Tabs">
-          <View style={{ marginTop: 20 }}>
-            <View style={{ marginBottom: 10 }}>
-              <PackenUiTabs
-                items={[
-                  {
-                    icon: "»",
-                    label: "Buttons",
-                    view: <Buttons />,
-                    callback: this.mockCallback,
-                  },
-                  {
-                    icon: "»",
-                    label: "Avatars",
-                    view: <Avatars />,
-                    callback: this.mockCallback
-                  },
-                  {
-                    icon: "»",
-                    label: "InfoActions",
-                    view: <InfoActions />,
-                    callback: this.mockCallback
-                  }
-                ]}
-                name="tabs1"
-                activeIndex={0}
-                onTabChange={this.onTabChange}
-              />
-            </View>
-            <View style={{ marginBottom: 10 }}>
-              <PackenUiTabs
-                items={[
-                  {
-                    label: "Button",
-                    callback: this.mockCallback
-                  },
-                  {
-                    label: "Button",
-                    callback: this.mockCallback
-                  },
-                  {
-                    label: "Button",
-                    callback: this.mockCallback
-                  }
-                ]}
-                name="tabs2"
-                activeIndex={1}
-                onTabChange={this.onTabChange}
-              />
-            </View>
-            <View style={{ marginBottom: 10 }}>
-              <PackenUiTabs
-                items={[
-                  {
-                    label: "Button",
-                    icon: "clock",
-                    callback: this.mockCallback
-                  },
-                  {
-                    label: "Button",
-                    icon: "rotate-cw",
-                    callback: this.mockCallback
-                  },
-                  {
-                    label: "Button",
-                    icon: "check-circle",
-                    callback: this.mockCallback
-                  }
-                ]}
-                name="tabs3"
-                activeIndex={2}
-                onTabChange={this.onTabChange}
-              />
-            </View>
-          </View>
-        </Section>
-      </PageView>
     );
   }
 }
