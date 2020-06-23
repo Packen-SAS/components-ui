a825e8e242e17138aa5e67698f00c1dbdd08244f
Andrés Liu <andres.liu@packen.co>
2020-06-23 12:33:43 -0500

    @andresliu(tests): Updated tests after refactors

--- __tests__/components/PackenUiCamera.test.js
+++ __tests__/components/PackenUiCamera.test.js
@@ -137,7 +137,7 @@ describe("<PackenUiCamera/>", () => {
       const prevProps = { test: "Test" };
       renderTopTriggers.setProps({ test: "Test 2", image: null });
       renderTopTriggersInstance.componentDidUpdate(prevProps);
-      expect(renderTopTriggersInstance.state.source.uri).toBe(null);
+      expect(renderTopTriggersInstance.state.source.uri).toBe("");
 
       renderTopTriggers.setProps({ test: "Test 2", image: { uri: "test" } });
       expect(renderTopTriggersInstance.state.source.uri).toBe("test");
