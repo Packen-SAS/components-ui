a1008b221939bbb3b182f7aba4651d5127dc075a
Andres David Liu Fong <andres.liu@packen.co>
2021-02-25 14:21:30 -0500

    refactor(calendar, map, panel, dependencies): add new dependency, remove map component since it needs access to driver project files, and add missing JSDoc annotations

--- app/components/PackenUiCalendar.tsx
+++ app/components/PackenUiCalendar.tsx
@@ -1,18 +1,68 @@
-import DateTimePicker from "@react-native-community/datetimepicker";
+import DateTimePicker, { Event, AndroidEvent } from "@react-native-community/datetimepicker";
 import { View, Modal, Platform, Appearance } from "react-native";
-import React, { Component } from "react";
+import React, { Component, ReactNode } from "react";
 
 import Colors from "../styles/abstracts/colors";
 import PackenUiText from "./PackenUiText";
-import * as UTIL from "../modules/utils";
+import * as UTIL from "../utils";
 
-export default class PackenUiCalendar extends Component {
-  constructor(props) {
+interface PackenUiCalendarProps {
+  mode: "date" | "time" | undefined;
+  label: string;
+  isOpen: boolean;
+  value: Date;
+  display: "calendar" | "default" | "compact" | "inline" | "spinner";
+  onChange: Function;
+  minimumDate: Date;
+  onRequestClose: Function;
+}
+
+interface PackenUiCalendarState {
+  mode: "date" | "time" | undefined;
+  label: string;
+  isOpen: boolean;
+  value: Date;
+  display: "calendar" | "default" | "compact" | "inline" | "spinner";
+  onChange: Function;
+  minimumDate: Date;
+  onRequestClose: Function;
+}
+
+type onChangeHandlerType = ((event: Event | AndroidEvent, date?: Date | undefined) => void);
+
+/**
+ * Component for rendering a calendar (modal on Android and spinner panel on iOS)
+ */
+export default class PackenUiCalendar extends Component<PackenUiCalendarProps, PackenUiCalendarState> {
+  /**
+   * Initializes the component
+   * @type {function}
+   * @param {object} props Props passed to the component
+   */
+  constructor(props: PackenUiCalendarProps) {
     super(props);
-    this.state = { ...this.setPropsToState() }
-  };
 
-  setPropsToState = () => ({
+    /**
+     * Variable that stores the state
+     * @type {object}
+     */
+    this.state = { ...this.setPropsToState() }
+  }
+
+  /**
+   * Centralizes the received props assignment to set them to the state, determining default values in case any is not provided
+   * @type {function}
+   * @property {string} [mode="date"] The "mode" prop for the DateTimePicker component
+   * @property {string} [label="OK"] The label to display on iOS to close the calendar panel
+   * @property {boolean} [isOpen=false] Whether the calendar should be displayed or not
+   * @property {Date} [value=new Date()] The selected date
+   * @property {string} [display="calendar"] The "display" prop for the DateTimePicker component
+   * @property {function} [onChange=mockFn]  The function to trigger when changing the selected date
+   * @property {Date} [minimumDate=new Date()] The optional minimum date to be able to be selected
+   * @property {function} [onRequestClose=mockFn] The function to trigger when closing the calendar
+   * @return {object} The props mapped to the state keys
+   */
+  setPropsToState: Function = (): PackenUiCalendarProps => ({
     mode: this.props.mode || "date",
     label: this.props.label || "OK",
     isOpen: this.props.isOpen || false,
@@ -23,24 +73,48 @@ export default class PackenUiCalendar extends Component {
     onRequestClose: this.props.onRequestClose || this.mockFn
   });
 
-  mockFn = () => false;
+  /**
+   * Placeholder function
+   * @type {function}
+   * @return {boolean} Returns false
+   */
+  mockFn: Function = (): false => false;
 
-  componentDidUpdate(prevProps) {
+  /**
+   * Compares props to determine if the component should update its state with new props
+   * @type {function}
+   * @param {object} prevProps Previous props
+   */
+  componentDidUpdate(prevProps: PackenUiCalendarProps) {
     if (!UTIL.objectsEqual(prevProps, this.props)) {
       this.setState({ ...this.setPropsToState() });
     }
   }
 
-  closePicker = () => { this.state.onRequestClose(); }
+  /**
+   * Closes the date picker by calling prop function
+   * @type {function}
+   */
+  closePicker: Function = () => { this.state.onRequestClose(); }
 
-  onChangeHandler = (_, date) => {
+  /**
+   * Event handler to be triggered after selecting a new date
+   * @param {object} _ Object containing the event data
+   * @param {Date | undefined} date The selected date
+   */
+  onChangeHandler: onChangeHandlerType = (_: Event | AndroidEvent, date?: Date | undefined) => {
     this.state.onChange(_, date);
     if (Platform.OS === "android") {
       this.closePicker();
     }
   }
 
-  getRender = () => {
+  /**
+   * Returns the main elements of the component
+   * @type {function}
+   * @return {node|null} JSX for the main elements or null if calendar is closed on Android
+   */
+  getRender: Function = (): ReactNode | null => {
     const main = (
       <DateTimePicker
         mode={this.state.mode}
@@ -77,14 +151,24 @@ export default class PackenUiCalendar extends Component {
     );
   }
 
-  getPanelBg = () => {
+  /**
+   * Returns the correct background color on iOS depending on the system appearance mode
+   * @type {function}
+   * @return {string} The background color to be applied to the calendar panel
+   */
+  getPanelBg: Function = (): string => {
     if (Appearance.getColorScheme() === "light") {
       return Colors.basic.white.dft;
     }
     return Colors.basic.independence.drk;
   }
 
-  getStyles = () => ({
+  /**
+   * Returns the current styles object
+   * @type {function}
+   * @return {object} The styles object
+   */
+  getStyles: Function = (): object => ({
     wrapper: {
       top: 0,
       left: 0,
@@ -110,7 +194,12 @@ export default class PackenUiCalendar extends Component {
     }
   });
 
-  render() {
+  /**
+   * Renders the component
+   * @type {function}
+   * @return {node} JSX for the component
+   */
+  render(): ReactNode {
     return this.getRender();
-  };
+  }
 }
--- app/components/PackenUiPanel.tsx
+++ app/components/PackenUiPanel.tsx
@@ -1,13 +1,48 @@
-import { PanGestureHandler } from "react-native-gesture-handler";
-import { View, Animated } from "react-native";
-import React, { Component } from "react";
+import { PanGestureHandler, PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import { View, Animated, LayoutChangeEvent } from "react-native";
+import React, { Component, ReactNode } from "react";
 
 import colors from "../styles/abstracts/colors";
 
-export default class PackenUiPanel extends Component {
-  constructor(props) {
+interface PackenUiPanelProps {
+  isHidden: boolean;
+  initialHeight: number;
+  onPanelReady: VoidFunction;
+  windowHeight: number;
+  windowFraction: number;
+  children: ReactNode | undefined | null;
+}
+
+interface PackenUiPanelState {
+  onPanelReady: VoidFunction;
+  panel: {
+    gotOriginal: boolean;
+    originalHeight: number | null;
+    height: number | Animated.AnimatedValue;
+  };
+}
+
+type panHandlerType = (e: PanGestureHandlerGestureEvent) => void;
+
+/**
+ * Component for rendering a vertical sliding panel with custom children inside
+ */
+export default class PackenUiPanel extends Component<PackenUiPanelProps, PackenUiPanelState> {
+  /**
+   * Initializes the component
+   * @type {function}
+   * @param {object} props Props passed to the component
+   */
+  constructor(props: PackenUiPanelProps) {
     super(props);
 
+    /**
+     * Variable that stores the state
+     * @type {object}
+     * @property {boolean} [panel.gotOriginal=false] Whether the initial height was stored
+     * @property {number|null} [originalHeight=number|null] The initial height value
+     * @property {function} [onPanelReady=this.ignoreAction] The optional function to trigger when component has finished loading
+     */
     this.state = {
       panel: {
         gotOriginal: false,
@@ -18,9 +53,19 @@ export default class PackenUiPanel extends Component {
     };
   }
 
-  ignoreAction = () => true;
+  /**
+   * Placeholder function
+   * @type {function}
+   * @return {boolean} Returns true
+   */
+  ignoreAction: Function = (): boolean => true;
 
-  getPanelHeight = (e) => {
+  /**
+   * Sets the initial animated height value for the panel
+   * @type {function}
+   * @param {object} e The event data object
+   */
+  getPanelHeight: Function = (e: LayoutChangeEvent) => {
     if (!this.state.panel.gotOriginal) {
       const { height } = e.nativeEvent.layout;
       this.setState({
@@ -33,18 +78,31 @@ export default class PackenUiPanel extends Component {
     }
   }
 
-  panHandler = (e) => {
-    const delta = e.nativeEvent.translationY * -1;
-    const newHeight = this.state.panel.height.__getValue() + delta;
-    if (
-      newHeight >= this.state.panel.originalHeight
-      && newHeight <= (this.props.windowHeight / this.props.windowFraction)
-    ) {
-      this.state.panel.height.setValue(newHeight);
+  /**
+   * Handles the pan event on the handler element
+   * @type {function}
+   * @param {object} e The event data object
+   */
+  panHandler: panHandlerType = (e: PanGestureHandlerGestureEvent) => {
+    if (typeof this.state.panel.height !== "number") {
+      const delta = e.nativeEvent.translationY * -1;
+      const newHeight = this.state.panel.height.__getValue() + delta;
+      if (
+        this.state.panel.originalHeight
+        && newHeight >= this.state.panel.originalHeight
+        && newHeight <= (this.props.windowHeight / this.props.windowFraction)
+      ) {
+        this.state.panel.height.setValue(newHeight);
+      }
     }
   }
 
-  render() {
+  /**
+   * Renders the component
+   * @type {function}
+   * @return {node} JSX for the component
+   */
+  render(): ReactNode {
     if (this.props.isHidden) { return null; }
     return (
       <View>
@@ -66,7 +124,12 @@ export default class PackenUiPanel extends Component {
     );
   }
 
-  getStyles = () => ({
+  /**
+   * Returns the current styles object
+   * @type {function}
+   * @return {object} The styles object
+   */
+  getStyles: Function = (): object => ({
     handleWrapper: {
       paddingVertical: 8,
       alignItems: "center",
