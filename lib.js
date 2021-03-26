3c423af60405a9a6189fb85ea06645596a722968
Andres David Liu Fong <andres.liu@packen.co>
2021-03-26 11:38:29 -0500

    refactor(lib): move components export to new file

---
+++ lib.js
@@ -0,0 +1,91 @@
+import PackenUiAlert from "./app/components/PackenUiAlert";
+import PackenUiAvatar from "./app/components/PackenUiAvatar";
+import PackenUiBadge from "./app/components/PackenUiBadge";
+import PackenUiButton from "./app/components/PackenUiButton";
+import PackenUiCalendar from "./app/components/PackenUiCalendar";
+import PackenUiCamera from "./app/components/PackenUiCamera";
+import PackenUiCheckbox from "./app/components/PackenUiCheckbox";
+import PackenUiCheckboxControl from "./app/components/PackenUiCheckboxControl";
+import PackenUiDivider from "./app/components/PackenUiDivider";
+import PackenUiDropdown from "./app/components/PackenUiDropdown";
+import PackenUiDropdownList from "./app/components/PackenUiDropdownList";
+import PackenUiDropdownListItem from "./app/components/PackenUiDropdownListItem";
+import PackenUiHeader from "./app/components/PackenUiHeader";
+import PackenUiInfoAction from "./app/components/PackenUiInfoAction";
+import PackenUiInput from "./app/components/PackenUiInput";
+import PackenUiInputBoxes from "./app/components/PackenUiInputBoxes";
+import PackenUiLicenseBox from "./app/components/PackenUiLicenseBox";
+import PackenUiList from "./app/components/PackenUiList";
+import PackenUiListItem from "./app/components/PackenUiListItem";
+import PackenUiLoaderButton from "./app/components/PackenUiLoaderButton";
+import PackenUiMap from "./app/components/PackenUiMap";
+import PackenUiMapPin from "./app/components/PackenUiMapPin";
+import PackenUiMapPinSub from "./app/components/PackenUiMapPinSub";
+import PackenUiModal from "./app/components/PackenUiModal";
+import PackenUiNotificationBanner from "./app/components/PackenUiNotificationBanner";
+import PackenUiPanel from "./app/components/PackenUiPanel";
+import PackenUiProgressbar from "./app/components/PackenUiProgressbar";
+import PackenUiRadar from "./app/components/PackenUiRadar";
+import PackenUiRadio from "./app/components/PackenUiRadio";
+import PackenUiRadioControl from "./app/components/PackenUiRadioControl";
+import PackenUiRating from "./app/components/PackenUiRating";
+import PackenUiSelectionButtons from "./app/components/PackenUiSelectionButtons";
+import PackenUiSelectionButtonsControl from "./app/components/PackenUiSelectionButtonsControl";
+import PackenUiServiceStatus from "./app/components/PackenUiServiceStatus";
+import PackenUiServiceStatusItem from "./app/components/PackenUiServiceStatusItem";
+import PackenUiShipmentCard from "./app/components/PackenUiShipmentCard";
+import PackenUiSvgIcon from "./app/components/PackenUiSvgIcon";
+import PackenUiTabs from "./app/components/PackenUiTabs";
+import PackenUiTabsItem from "./app/components/PackenUiTabsItem";
+import PackenUiTag from "./app/components/PackenUiTag";
+import PackenUiText from "./app/components/PackenUiText";
+import PackenUiToggle from "./app/components/PackenUiToggle";
+import PackenUiVehicleBox from "./app/components/PackenUiVehicleBox";
+import PackenUiWhatsAppLink from "./app/components/PackenUiWhatsAppLink";
+
+export {
+  PackenUiAlert,
+  PackenUiAvatar,
+  PackenUiBadge,
+  PackenUiButton,
+  PackenUiCalendar,
+  PackenUiCamera,
+  PackenUiCheckbox,
+  PackenUiCheckboxControl,
+  PackenUiDivider,
+  PackenUiDropdown,
+  PackenUiDropdownList,
+  PackenUiDropdownListItem,
+  PackenUiHeader,
+  PackenUiInfoAction,
+  PackenUiInput,
+  PackenUiInputBoxes,
+  PackenUiLicenseBox,
+  PackenUiList,
+  PackenUiListItem,
+  PackenUiLoaderButton,
+  PackenUiMap,
+  PackenUiMapPin,
+  PackenUiMapPinSub,
+  PackenUiModal,
+  PackenUiNotificationBanner,
+  PackenUiPanel,
+  PackenUiProgressbar,
+  PackenUiRadar,
+  PackenUiRadio,
+  PackenUiRadioControl,
+  PackenUiRating,
+  PackenUiSelectionButtons,
+  PackenUiSelectionButtonsControl,
+  PackenUiServiceStatus,
+  PackenUiServiceStatusItem,
+  PackenUiShipmentCard,
+  PackenUiSvgIcon,
+  PackenUiTabs,
+  PackenUiTabsItem,
+  PackenUiTag,
+  PackenUiText,
+  PackenUiToggle,
+  PackenUiVehicleBox,
+  PackenUiWhatsAppLink,
+};
